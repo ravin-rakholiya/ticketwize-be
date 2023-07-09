20677b5a8f1de14f6d52b746d7ed6a265b1987fc
ravin-rakholiya <ravinkumarrakh@gmail.com>
2023-07-08 21:59:08 -0400

    changed the business logic of payment

--- payment/scripts.py
+++ payment/scripts.py
@@ -14,7 +14,8 @@ def checkout_payment(user_event, email):
 	event_fees = user_event.event.price
 	service_fees = user_event.event.payment_config.service_fee
 	payment_charge = user_event.event.payment_config.payment_fee
-	total_fees = ((no_of_tickets*(event_fees) + (no_of_tickets*((event_fees*(service_fees+payment_charge))/100))))*100
+	flat_fee = user_event.event.payment_config.flat_fee
+	total_fees = ((no_of_tickets*(event_fees + flat_fee) + (no_of_tickets*((event_fees*(service_fees+payment_charge))/100))))*100
 	success_url = settings.SITE_URL+"/components/blocks/Payment/PaymentSuccess/?success=true&session_id={CHECKOUT_SESSION_ID}"+"&email="+email
 	cancle_url = settings.SITE_URL+"/components/blocks/Payment/PaymentCancle/?canceled=true"+"&email="+email
 	transaction_details = {"user_event_id":user_event.id,"payment_method_type":payment_method_type, "no_of_tickets":no_of_tickets, "mode": mode, "currency":currency, "product_data":product_data, "unit_amount":total_fees}
