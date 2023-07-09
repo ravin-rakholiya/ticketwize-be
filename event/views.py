58a658e16d4466371acd33a8f3d93c629e022e81
ravin-rakholiya <ravinkumarrakh@gmail.com>
2023-07-09 00:04:04 -0400

    added print

--- event/views.py
+++ event/views.py
@@ -59,13 +59,7 @@ class RegisterEventAPIView(APIView):
 					print(f"56-------")
 					user = user.last()
 					print(f"57-------")
-				else:
-					print(f"58-------")
-					user = User.objects.filter(contact_number = contact_number)
-					print(f"59-------")
-					if user:
-						print(f"60-------")
-						user = user.last()
+
 				if user is None:
 					print(f"61-------")
 					user = User.objects.create(first_name = first_name, last_name = last_name, email = email, contact_number = contact_number, gender = gender)
